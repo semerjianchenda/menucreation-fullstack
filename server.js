deddd650b55ed8185bc43dd7d78a749c1327b116
Chenda Semerjian-Krants <96898870+semerjianchenda@users.noreply.github.com>
2024-11-17 17:19:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -52,3 +52,4 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
+
